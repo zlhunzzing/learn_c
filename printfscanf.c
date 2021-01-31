c4a15b6b0177ad3d7331cc303596d6482e087bbe
zlhunzzing <zlhunzzing@gmail.com>
2021-01-31 16:02:17 +0900

    실수형 변수

--- printfscanf.c
+++ printfscanf.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 int main(void)
 {
-	int age = 12;
+	// 정수형 변수에 대한 예제
+	/*int age = 12;
 	printf("%d\n", age);
 	age = 13;
-	printf("%d\n", age);
+	printf("%d\n", age);*/
+
+	// 실수형 변수에 대한 예제
+	//float f = 46.5f;
+	//printf("%.2f\n", f);
+	//double d = 4.428;
+	//printf("%.2f\n", d);
 	return 0;
 }

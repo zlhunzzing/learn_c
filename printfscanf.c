ee444849fe26d2bd5ea1084c4138cd6679e42a2e
zlhunzzing <zlhunzzing@gmail.com>
2021-02-04 23:43:19 +0900

    scanf

--- printfscanf.c
+++ printfscanf.c
@@ -18,9 +18,31 @@ int main(void)
 
 	//printf
 	// 연산
-	int add = 3 + 7; // 10
+	//int add = 3 + 7; // 10
 	//printf("3 + 7 = %d\n", add);
-	printf("%d x %d = %d\n", 30, 79, 30 * 79);
+	//printf("%d x %d = %d\n", 30, 79, 30 * 79);
+
+	// scanf
+	// 키보드 입력을 받아서 저장
+	//int input;
+	//printf("값을 입력하세요 : ");
+	//scanf_s("%d", &input);
+	//printf("입력값 : %d\n", input);*/
+
+	/*int one, two, three;
+	printf("3개의 정수를 입력하세요 : ");
+	scanf_s("%d %d %d", &one, &two, &three);
+	printf("첫번째 값 : %d\n", one);
+	printf("두번째 값 : %d\n", two);
+	printf("세번째 값 : %d\n", three);*/
+
+	// 문자(한 글자), 문자열(한 글자 이상의 여러 글자)
+	//char c = 'A';
+	//printf("%c\n", c);
+
+	char str[256];
+	scanf_s("%s", str, sizeof(str));
+	printf("%s\n", str);
 
 	return 0;
 }

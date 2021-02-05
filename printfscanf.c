95623818262bc721941cb0650fae79473aaca774
zlhunzzing <zlhunzzing@gmail.com>
2021-02-05 17:28:23 +0900

    조서쓰기

--- printfscanf.c
+++ printfscanf.c
@@ -40,9 +40,40 @@ int main(void)
 	//char c = 'A';
 	//printf("%c\n", c);
 
-	char str[256];
+	/*char str[256];
 	scanf_s("%s", str, sizeof(str));
-	printf("%s\n", str);
+	printf("%s\n", str);*/
+
+	// 프로젝트
+	// 경찰관이 범죄자의 정보를 입수 (조서 작성)
+	// 이름? 나이? 몸무게? 키? 범죄명?
+	char name[256];
+	printf("이름이 뭐예요? ");
+	scanf_s("%s", name, sizeof(name));
+
+	int age;
+	printf("몇살이에요? ");
+	scanf_s("%d", &age);
+
+	float weight;
+	printf("몸무게는 몇 kg 이예요? ");
+	scanf_s("%f", &weight);
+
+	double height;
+	printf("키는 몇 cm 이예요? ");
+	scanf_s("%lf", &height);
+	
+	char what[256];
+	printf("무슨 범죄를 저질렀어요? ");
+	scanf_s("%s", what, sizeof(what));
+
+	//// 조서 내용 출력
+	printf("\n\n--- 범죄자 정보 ---\n\n");
+	printf(" 이름       : %s\n", name);
+	printf(" 나이       : %d\n", age);
+	printf(" 몸무게     : %.2f\n", weight);
+	printf(" 키         : %.2lf\n", height);
+	printf(" 범죄       : %s\n", what);
 
 	return 0;
 }

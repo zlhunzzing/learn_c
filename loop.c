e5a861aa863a7e887e6f71e2bef159b9595b84cd
zlhunzzing <zlhunzzing@gmail.com>
2021-02-06 20:42:21 +0900

    ++

---
+++ loop.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+int main(void)
+{
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	//printf("Hello World\n");
+	
+	// ++ ปÜปÜ
+	//int a = 10;
+	//printf("a ดย %d\n", a);
+	//a++;
+	//printf("a ดย %d\n", a);
+	//a++;
+	//printf("a ดย %d\n", a);
+
+	//int b = 20;
+	//// b + b + 1;
+	//printf("bดย %d\n", ++b);
+	//printf("bดย %d\n", b++);
+	//printf("bดย %d\n", b);
+
+	int i = 1;
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+	printf("Hello World %d\n", i++);
+
+	return 0;
+}

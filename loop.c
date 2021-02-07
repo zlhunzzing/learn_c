ed6c357960b272e063d80128c615cf760cf22e9e
zlhunzzing <zlhunzzing@gmail.com>
2021-02-07 13:13:32 +0900

    for

--- loop.c
+++ loop.c
@@ -27,17 +27,26 @@ int main(void)
 	//printf("b는 %d\n", b++);
 	//printf("b는 %d\n", b);
 
-	int i = 1;
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
-	printf("Hello World %d\n", i++);
+	//int i = 1;
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+	//printf("Hello World %d\n", i++);
+
+	// 반복문
+	// for, while, do while
+
+	// for (선언; 조건; 증감)
+	for (int i = 1; i <= 10; i++)
+	{
+		printf("Hello World %d\n", i);
+	}
 
 	return 0;
 }

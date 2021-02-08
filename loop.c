79e0206e0fa9f077c8a180b02aeea466843c2525
zlhunzzing <zlhunzzing@gmail.com>
2021-02-08 12:20:02 +0900

    while

--- loop.c
+++ loop.c
@@ -43,9 +43,17 @@ int main(void)
 	// for, while, do while
 
 	// for (선언; 조건; 증감)
-	for (int i = 1; i <= 10; i++)
+	/*for (int i = 1; i <= 10; i++)
 	{
 		printf("Hello World %d\n", i);
+	}*/
+
+	// while (조건) { }
+	int i = 1;
+	while (i <= 10)
+	{
+		printf("Hello World %d\n", i++);
+		//i++;
 	}
 
 	return 0;

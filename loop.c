345655feb0e87828ca53e7dbde8146214d824031
zlhunzzing <zlhunzzing@gmail.com>
2021-02-15 20:26:23 +0900

    거꾸로 별

--- loop.c
+++ loop.c
@@ -96,9 +96,30 @@ int main(void)
 	****
 	*****
 	*/
-	for (int i = 0; i < 5; i++)
+	/*for (int i = 0; i < 5; i++)
 	{
 		for (int j = 0; j <= i; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}*/
+
+    /*
+	SSSS*
+	SSS**
+	SS***
+	S****
+	*****
+	*/
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = i; j < 5 - 1; j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k <= i; k++)
 		{
 			printf("*");
 		}

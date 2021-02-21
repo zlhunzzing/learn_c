dc56de095cf366ff187801debc68434b868313f5
zlhunzzing <zlhunzzing@gmail.com>
2021-02-21 12:35:06 +0900

    switch case

--- condition.c
+++ condition.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <time.h>
+
 
 int main(void)
 {
@@ -62,17 +64,66 @@ int main(void)
 	//}
 
 	// && ||
-	int a = 10;
-	int b = 11;
-	int c = 13;
-	int d = 13;
-	if (a == b || c == d)
+	//int a = 10;
+	//int b = 11;
+	//int c = 13;
+	//int d = 13;
+	//if (a == b || c == d)
+	//{
+	//	printf("a 와 b, 혹은 c 와 d 의 값이 같습니다\n");
+	//}
+	//else
+	//{
+	//	printf("값이 서로 다르네요");
+	//}
+
+	// 가위 0 바위 1 보 2
+	//srand(time(NULL));
+	//int i = rand() & 3; // 0 ~ 2 반환
+	/*if (i == 0)
 	{
-		printf("a 와 b, 혹은 c 와 d 의 값이 같습니다\n");
+		printf("가위\n");
+	}
+	else if (i == 1)
+	{
+		printf("바위\n");
+	}
+	else if (i == 2)
+	{
+		printf("보\n");
 	}
 	else
 	{
-		printf("값이 서로 다르네요");
+		printf("몰라요");
+	}*/
+
+	//srand(time(NULL));
+	////int i = rand() & 3; // 0 ~ 2 반환
+	//int i = 1;
+	//switch (i)
+	//{
+	//case 0: printf("가위\n"); break;
+	//case 1: printf("바위\n"); break;
+	//case 2: printf("보\n"); break;
+	//default:printf("몰라요"); break;
+	//}
+
+	int age = 17;
+	switch (age)
+	{
+	case 8:
+	case 9:
+	case 10:
+	case 11:
+	case 12:
+	case 13:printf("초등학생입니다\n"); break;
+	case 14:
+	case 15:
+	case 16:printf("중학생입니다\n"); break;
+	case 17:
+	case 18:
+	case 19:printf("고등학생입니다\n"); break;
+	default: printf("학생이 아닌가봐요\n"); break;
 	}
 
 	return 0;

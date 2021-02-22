db2206fa996d8c480936ba9c43bcb1b17381e0c7
zlhunzzing <zlhunzzing@gmail.com>
2021-02-22 22:00:00 +0900

    숫자 맞히기

--- condition.c
+++ condition.c
@@ -108,22 +108,59 @@ int main(void)
 	//default:printf("몰라요"); break;
 	//}
 
-	int age = 17;
-	switch (age)
+	//int age = 17;
+	//switch (age)
+	//{
+	//case 8:
+	//case 9:
+	//case 10:
+	//case 11:
+	//case 12:
+	//case 13:printf("초등학생입니다\n"); break;
+	//case 14:
+	//case 15:
+	//case 16:printf("중학생입니다\n"); break;
+	//case 17:
+	//case 18:
+	//case 19:printf("고등학생입니다\n"); break;
+	//default: printf("학생이 아닌가봐요\n"); break;
+	//}
+
+	// Up and Down
+	srand(time(NULL));
+	int num = rand() % 100 + 1; // 1 ~ 100 사이의 숫자
+	printf("숫자 : %d\n", num);
+	int answer = 0; // 정답
+	int chance = 5; // 기회
+	while (1) // 1 : 참, 0 : 거짓
 	{
-	case 8:
-	case 9:
-	case 10:
-	case 11:
-	case 12:
-	case 13:printf("초등학생입니다\n"); break;
-	case 14:
-	case 15:
-	case 16:printf("중학생입니다\n"); break;
-	case 17:
-	case 18:
-	case 19:printf("고등학생입니다\n"); break;
-	default: printf("학생이 아닌가봐요\n"); break;
+		printf("남은 기회 %d 번\n", chance--);
+		printf("숫자를 맞혀보세요 (1~100) : ");
+		scanf_s("%d", &answer);
+
+		if (answer > num)
+		{
+			printf("Down ↓\n\n");
+		}
+		else if (answer < num)
+		{
+			printf("UP ↑\n\n");
+		}
+		else if (answer == num)
+		{
+			printf("정답입니다 !\n\n");
+			break;
+		}
+		else
+		{
+			printf("알 수 없는 오류가 발생했어요\n\n");
+		}
+
+		if (chance == 0)
+		{
+			printf("모든 기회를 다 사용하셨네요. 아쉽게 실패했습니다\n");
+			break;
+		}
 	}
 
 	return 0;

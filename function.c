b60702cbb4e1b0606e6356615eb4f009ce8ff3ea
zlhunzzing <zlhunzzing@gmail.com>
2021-02-23 12:42:35 +0900

    함수

---
+++ function.c
@@ -0,0 +1,42 @@
+#include <stdint.h>>
+
+// 선언
+void p(int num);
+
+int main(void)
+{
+	// function
+	// 계산기
+
+	int num = 2;
+	//printf("num은 %d 입니다\n", num); // 2
+	p(num);
+
+	// 2 + 3 은?
+	num = num + 3; // num += 3;
+	//printf("num 은 %d 입니다\n", num); // 5
+	p(num);
+
+	// 5 - 1 은?
+	num -= 1; // num = num -1;
+	//printf("num 은 %d 입니다\n", num); // 5
+	p(num);
+
+	// 4 x 3 은 ?
+	num *= 3;
+	//printf("num 은 %d 입니다\n", num); // 12
+	p(num);
+
+	// 12 / 6 은 ?
+	num /= 6;
+	//printf("num 은 %d 입니다\n", num); // 2
+	p(num);
+
+	return 0;
+}
+
+// 정의
+void p(int num)
+{
+	printf("num 은 %d 입니다\n", num);
+}

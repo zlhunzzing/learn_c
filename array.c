b2c79252e6810d5b4aede8f0eb11a49c2545ca72
zlhunzzing <zlhunzzing@gmail.com>
2021-03-17 09:43:01 +0900

    아스키 코드

--- array.c
+++ array.c
@@ -96,10 +96,40 @@ int main(void)
 	//}
 
 	// 문자열 입력받기 : 경찰서 조서 쓰기 예제
-	char name[256];
+	/*char name[256];
 	printf("이름이 뭐예요? ");
 	scanf_s("%s", name, sizeof(name));
-	printf("%s\n", name);
+	printf("%s\n", name);*/
+
+	// 참고 : ASCII 코드 ? ANSI(미국표준협회) 에서 제시한 표준 코드 체계
+	// 7bit, 총 128개 코드 (0-127)
+	// a : 97 (문자 a 의 아스키코드 정수값)
+	// A : 65
+	// 0 : 48
+
+	//printf("%c\n", 'a');
+	//printf("%d\n", 'a');
+
+	//printf("%c\n", 'b');
+	//printf("%d\n", 'b');
+
+	//printf("%c\n", 'A');
+	//printf("%d\n", 'A');
+
+	//printf("%c\n", '\0');
+	//printf("%d\n", '\0');
+
+	//printf("%c\n", '0');
+	//printf("%d\n", '0');
+
+	//printf("%c\n", '1');
+	//printf("%d\n", '1');
+
+	// 참고2 : 0-127 사이의 아스키코드 정수값에 해당하는 문자 확인
+	for (int i = 0; i < 128; i++)
+	{
+		printf("아스키코드 정수 %d : %c\n", i, i);
+	}
 
 	return 0;
 }

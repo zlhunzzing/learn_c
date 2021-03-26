1c9bc73f6b78a0f396311e6ef402079d7f9fc50e
zlhunzzing <zlhunzzing@gmail.com>
2021-03-26 10:59:35 +0900

    포인터로 배열 수정

--- pointer.c
+++ pointer.c
@@ -2,6 +2,7 @@
 
 void swap(int a, int b);
 void swap_addr(int * a, int * b);
+void changeArray(int* ptr);
 
 int main(void)
 {
@@ -113,22 +114,33 @@ int main(void)
 	*/
 
 	// SWAP
-	int a = 10;
-	int b = 20;
-	printf("a 의 주소 : %d\n", &a);
-	printf("b 의 주소 : %d\n", &b);
+	//int a = 10;
+	//int b = 20;
+	//printf("a 의 주소 : %d\n", &a);
+	//printf("b 의 주소 : %d\n", &b);
 
-	// a 와 b 의 값을 바꾼다
-	printf("Swqp 함수 전 => a : %d, b : %d\n", a, b);
-	swap(a, b);
-	printf("Swqp 함수 후 => a : %d, b : %d\n", a, b);
+	//// a 와 b 의 값을 바꾼다
+	//printf("Swqp 함수 전 => a : %d, b : %d\n", a, b);
+	//swap(a, b);
+	//printf("Swqp 함수 후 => a : %d, b : %d\n", a, b);
 
-	// 값에 의한 복사 (Call by Value) -> 값만 복사한다는 의미
+	//// 값에 의한 복사 (Call by Value) -> 값만 복사한다는 의미
 
-	// 주소값을 넘기면? 메모리 공간에 있는 주소값 자체를 넘기면... 철수네처럼
-	printf("(주소값 전달)Swqp 함수 전 => a : %d, b : %d\n", a, b);
-	swap_addr(&a, &b);
-	printf("(주소값 전달)Swqp 함수 후 => a : %d, b : %d\n", a, b);
+	//// 주소값을 넘기면? 메모리 공간에 있는 주소값 자체를 넘기면... 철수네처럼
+	//printf("(주소값 전달)Swqp 함수 전 => a : %d, b : %d\n", a, b);
+	//swap_addr(&a, &b);
+	//printf("(주소값 전달)Swqp 함수 후 => a : %d, b : %d\n", a, b);
+
+	int arr2[3] = { 10,20,30 };
+	/*changeArray(arr2);*/
+	changeArray(&arr2);
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%d\n", arr2[i]);
+
+	}
+
+	// scanf 에서 &num 과 같이 & 를 사용하는 이유?
 
 	return 0;
 }
@@ -150,4 +162,9 @@ void swap_addr(int * a, int * b)
 	*a = *b;
 	*b = temp;
 	printf("(주소값 전달)Swqp 함수 후 => a : %d, b : %d\n", *a, *b);
+}
+
+void changeArray(int* ptr)
+{
+	ptr[2] = 50;
 }

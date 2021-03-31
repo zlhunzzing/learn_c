e1053f9220e1c984723f4762ce4e01b8dacc3de9
zlhunzzing <zlhunzzing@gmail.com>
2021-03-31 11:23:17 +0900

    다차원 배열

---
+++ m_array.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// 다차원 배열 Multidimensional Array
+	int i; // ㅁ
+
+	int arr[5];
+	// ㅁㅁㅁㅁㅁ
+
+	int arr2[2][5];
+	// ㅁㅁㅁㅁㅁ
+	// ㅁㅁㅁㅁㅁ
+
+	int arr3[4][2];
+	// ㅁㅁ
+	// ㅁㅁ
+	// ㅁㅁ
+	// ㅁㅁ
+
+	int arr4[3][3][3];
+	// ㅁㅁㅁ
+	// ㅁㅁㅁ
+	// ㅁㅁㅁ
+
+	// ㅁㅁㅁ
+	// ㅁㅁㅁ
+	// ㅁㅁㅁ
+
+	// ㅁㅁㅁ
+	// ㅁㅁㅁ
+	// ㅁㅁㅁ
+
+	return 0;
+}

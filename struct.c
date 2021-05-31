c918f199bc35654dca528787dacb9d82d98c7f0c
Jihun Kim <zlhunzzing@gmail.com>
2021-06-01 02:42:25 +0900

    typedef

--- struct.c
+++ struct.c
@@ -5,7 +5,18 @@ struct  GameInfo {
 	int year;
 	int price;
 	char* company;
+
+	struct GameInfo* friendGame; // 연관 업체 게임
 };
+
+typedef struct {
+	char* name;
+	int year;
+	int price;
+	char* company;
+
+	struct GameInfo* friendGame; // 연관 업체 게임
+} GAME_INFO;
 
 int main(void)
 {
@@ -71,5 +82,27 @@ int main(void)
 	printf("  발매년도: %d\n", gamePtr->year);
 	printf("  가격    : %d\n", gamePtr->price);
 	printf("  제작사  : %s\n", gamePtr->company);
+
+	// typedef
+	// 자료형에 별명 지정
+	int i = 1;
+	typedef int 정수;
+	정수 정수변수 = 3; // int i = 3;
+	typedef float 실수;
+	실수 실수변수 = 3.23f; // float f = 3.23f;
+	printf("\n\n정수변수 : %d, 실수변수 %.2f\n\n", 정수변수, 실수변수);
+
+	typedef struct GameInfo 게임정보;
+	게임정보 game1;
+	game1.name = " 한글 게임";
+	game1.year = 2015;
+
+	GAME_INFO game2;
+	game2.name = "한글 게임";
+	game2.year = 2014;
+
+	//struct  GameInfomation game3;
+	//game3.name = "한글 게임";
+
 	return 0;
 }
